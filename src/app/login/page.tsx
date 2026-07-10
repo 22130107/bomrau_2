4986219853043bdf03d208f254b1766c00e40276
22130107 <22130107@st.hcmuaf.edu.vn>
2026-07-10 17:32:03 +0700

    123

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,7 @@
 import { AdminLoginForm } from "@/components/AdminLoginForm";
 
+export const dynamic = "force-dynamic";
+
 export default function LoginPage() {
   return (
     <div className="min-h-screen flex items-center justify-center pt-10 pb-10 bg-[rgb(15,23,42)] px-4">
